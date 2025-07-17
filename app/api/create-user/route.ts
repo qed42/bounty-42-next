f73d2c9ac06d9d4039f7c9628beebb5fcd4ffc02
RChaubey16 <ruturajchaubey16@gmail.com>
2025-07-17 15:23:12 +0530

    feat: restrict login to only QED42 users.

--- app/api/create-user/route.ts
+++ app/api/create-user/route.ts
@@ -4,6 +4,11 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const { name, email } = await req.json();
+
+  if (!email.endsWith("@qed42.com")) {
+    return NextResponse.json({ error: "Incorrect email" }, { status: 400 });
+  }
+
   const [user] = await db.insert(users).values({ name, email }).returning();
   return NextResponse.json({ message: "User created", user });
 }
